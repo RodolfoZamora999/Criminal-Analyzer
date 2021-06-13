bb31b7cd68c50d99ec734544c822d652f7240290
Rodolfo Zamora <rodolfozamora999@gmail.com>
2021-06-12 17:06:25 -0700

    Update code. I added a new file 'Init.py' as program entry point

---
+++ Init.py
@@ -0,0 +1,18 @@
+import os
+from Main import main
+
+#Se resuleven las dependencias del proyecto
+def dependencies_resolution():
+    os.system("pip install termcolor")
+    os.system("clear")
+
+#Activa el color de la terminal (si aplica)
+def active_color_terminal():
+    os.system("color")
+    os.system("clear")
+
+#Punto de entrada del programa
+if __name__ == "__main__":
+    dependencies_resolution()
+    active_color_terminal()
+    main()
--- Main.py
+++ Main.py
@@ -7,62 +7,103 @@ file = "database.pl"
 #ejecuta "main" como objetivo y despues finaliza.
 init_query_prolog = "swipl -f {0} -g main -t halt".format(file)
 
-
-#Método de utilidad para resolver dependencias del proyecto.
-def dependencies_resolution():
-    os.system("pip install termcolor")
-    os.system("clear")
-
 #Método de utilidad para imprimir tablas con datos en consola.
-def print_table(values: iter):
+def print_table(values: iter, numerate: bool = False):
     max = 0
     for value in values:
         if len(value) > max:
             max = len(value)
-    
+        
     #Imprimir parte superior
-    print( "+" + ("-" * (max + 2)) + "+")
+    if numerate:
+        count_values = len(values)
+        add_v = 0
+        if count_values < 10:
+            add_v = 4
+        elif count_values >= 10 and count_values < 100:
+            add_v = 5 
+        elif count_values >= 100 and count_values < 1000:
+            add_v = 6 
+
+        print( "+" + ("-" * (max + 2 + add_v)) + "+")
+    else:
+        print( "+" + ("-" * (max + 2)) + "+")
     
     #Imprimir los valores de la tabla
+    cont_value = 0
     for value in values:
+        cont_value += 1
         if len(value) < max:
             diff = max - len(value)
             for add in range(diff):
                 value += " "
 
+        if numerate:
+            count_values = len(values)
+            if count_values >= 10 and count_values < 100:
+                if cont_value < 10:
+                    value = value + " "   
+            elif count_values >= 100 and count_values < 1000:
+                if cont_value < 10:
+                    value = value + "  "
+                elif cont_value >= 10 and cont_value < 100:
+                    value = value + " "
+
+            value =  "[{0}] ".format(cont_value) + value
+
         print("| ", value, " |", sep="")
-    
+
     #Imprimir parte inferior
-    print( "+" + ("-" * (max + 2)) + "+")
+    if numerate:
+        count_values = len(values)
+        add_v = 0
+        if count_values < 10:
+            add_v = 4
+        elif count_values >= 10 and count_values < 100:
+            add_v = 5 
+        elif count_values >= 100 and count_values < 1000:
+            add_v = 6 
+
+        print( "+" + ("-" * (max + 2 + add_v)) + "+")
+    else:
+        print( "+" + ("-" * (max + 2)) + "+")
 
 
 #Este método es responsable de cargar los datos a la base de conocimientos
-def input_database():
+def input_data():
     os.system("clear")
     print("Inserte las características del criminal.")
-    print("Sintaxis: caracteristica:valor")
-    print()
+    print("Escriba {0} para finalizar la entrada de características.".format(colored("complete", "green")))
+    print("{0}: caracteristica{1}valor\n".format(colored("Sintaxis", "yellow"), colored(":", "green")))
 
-    values = []
     #Inicia la carga de datos
+    values = []
     while True:
-        str_input = input("Insertar: ").lower().lstrip().rstrip().replace(" ", "_")
-        
+        str_input = input("{0} Característica: ".format(colored("[-]", "blue"))).lower().lstrip().rstrip().replace(" ", "_")
         if str_input == "complete" or str_input == "exit":
-            os.system("clear")
-            print("Valores que se almacenaran:")
-            print_table(values)
-
-            print("¿Desea guardarlos? 1:Sí 2:No")
-            save = input("Insertar: ")
-
-            if save == "1":
-                print("Datos almacenados")
-            elif save == "2":
-                print("Nada")
-            else:
-                print("Inserta un opción correcta!")
+            print("\nLas siguientes características se almacenarán en la base de conocimientos.")
+            print_table(values, numerate=True)
+            print("\n¿Confirma que desea almacenar estas características?")
+            print("{0} Sí, almacenar datos.".format(colored("[1]", "green")))
+            print("{0} No, seguir agregando características.".format(colored("[2]", "green")))
+            print("{0} Cancelar e ignorar datos.\n".format(colored("[3]", "green")))
             
+            while True:
+                select = input("{0} Seleccione una opción: ".format(colored("[-]", "blue")))
+                if select == "1":
+                    #Almacenar los datos en la base de conocimiento.
+                    print("Datos almacenados")
+                    break
+                elif select == "2":
+                    #Seguir con la entrada de datos.
+                    print("Nada")
+                    break
+                elif select == "3":
+                    #Ignora los datos y regresa al inicio.
+                    main()
+                    break
+                else:
+                    print("Por favor seleccione una opción correcta, vuelva a intentarlo.\n")
             break
 
         if str_input.find(":") == -1:
@@ -78,11 +119,9 @@ def query_criminal():
 
 #Método de entrada del programa
 def main():
-    #Se resuelven las dependencias
-    dependencies_resolution()
     os.system("clear")
     os.system("color")
-    print_table(["#"*40, "Bienvenidos a Criminal Analyzer 1.0", "#"*40])
+    print_table(["#"*40, "Bienvenidos a Criminal Analyzer 0.1", "#"*40])
     
     if os.path.exists(file):
         print(colored("Se ha detectado una base de datos existente.\n", "green"))
@@ -90,18 +129,21 @@ def main():
         print(colored("No se ha detectado una base de datos existente.\n", "red"))
 
     print("Por favor seleccione una de las siguientes opciones:")
-    print("{0} Introducir datos\n{1} Hacer una consulta\n".format(colored("[1]", "green"), colored("[2]", "green")))
-    select = input("{0} Selecciona una opción: ".format(colored("[-]", "blue")))
-    if select == "1":
-        input_database()
-    elif select == "2":
-        if os.path.exists(file):
-            query_criminal()
-        else:
-            print("No se ha detectado una base de datos")
-    else:
-        print("Por favor seleccione una opción correcta")
+    print("{0} Introducir datos.\n{1} Hacer una consulta.\n{2} Salir.\n".format(colored("[1]", "green"), colored("[2]", "green"), colored("[3]", "green")))
 
-#Punto de entrada del programa
-if __name__ == "__main__":
-    main()
+    while True:
+        select = input("{0} Seleccione una opción: ".format(colored("[-]", "blue")))
+        if select == "1":
+            input_data()
+            break
+        elif select == "2":
+            if os.path.exists(file):
+                query_criminal()
+                break
+            else:
+                print("No se ha detectado una base de datos existente, por favor registre los datos primero.\n")
+        elif select == "3":
+            os.system("clear")
+            exit()
+        else:
+            print("Por favor seleccione una opción correcta, vuelva a intentarlo.\n")
